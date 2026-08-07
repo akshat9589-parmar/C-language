b813cd3b5400f967231317b4d87995b0e6609700
akshat9589-parmar <mrakshatparmar@gmail.com>
2026-08-07 12:47:30 +0000

    Add Factorial question file

---
+++ Factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+     int n;           // We have to print the factorial of n
+     int factorial =1;
+
+  for(int i =1; i <=a; i++) {
+           factorial *= i;
+  }   
+  printf("Factorial: %d",factorial);
+  
+    return 0;
+}
